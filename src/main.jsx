5962f3271102feca9851f96d437ed4bf8f427a02
zakariahasanabdiali <zakihasanabdu443@gmail.com>
2025-08-24 11:30:39 +0300

    fix: router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter basename="/zakarie-portfolio">
     <App />
   </BrowserRouter>
 );
